715a3eead0698ef90d3f06427b038bdc232119fc
Averdrian <adrestev@ucm.es>
2021-03-14 13:16:52 +0100

    implementacion manual

---
+++ analizadorLexico(implementacion manual)/alex/ClaseLexica.java
@@ -0,0 +1,7 @@
+package alex;
+
+public enum ClaseLexica {
+ EntNum, RealNum, Variable, True, False, Bool, Int, 
+ Real, Sum, Res, Mul, Div, Asig, Menor, Mayor, Ig,
+ MayIg, MenIg, Dist, And, Or, Not, PAp, PCie, PunCo, Sep, EOF
+}
